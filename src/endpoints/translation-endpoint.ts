0eb776dfc359a8b908d54e52d10a689ee8960401
JayJayDee <jindongp@gmail.com>
2019-04-03 16:19:42 +0900

    added parameter validation to GET /translate API.

--- src/endpoints/translation-endpoint.ts
+++ src/endpoints/translation-endpoint.ts
@@ -1,6 +1,8 @@
 import { injectable } from 'smart-factory';
+import { isArray } from 'util';
 import { EndpointModules } from './modules';
 import { EndpointTypes } from './types';
+import { InvalidParamError } from '../errors';
 
 injectable(EndpointModules.Translation.Translate,
   [ EndpointModules.Utils.WrapAync ],
@@ -9,6 +11,22 @@ injectable(EndpointModules.Translation.Translate,
     method: EndpointTypes.EndpointMethod.GET,
     handler: [
       wrapAsync((req, res, next) => {
+        const queriesExpr = req.query.query;
+        const from = req.query.from;
+        const to = req.query.to;
+        let queries = null;
+
+        if (!queriesExpr || !from || !to) {
+          throw new InvalidParamError('query required');
+        }
+
+        try {
+          queries = JSON.parse(queriesExpr);
+          if (isArray(queries) === false) throw new Error('');
+        } catch (err) {
+          throw new InvalidParamError('query must be a stringified json-array');
+        }
+
         res.status(200).json({});
       })
     ]

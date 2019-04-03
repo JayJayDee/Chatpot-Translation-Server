e46106dae173ddb1a27165bc7fa2f90ffbe8b988
JayJayDee <jindongp@gmail.com>
2019-04-03 16:50:52 +0900

    applied translator implementation to endpoint. (draft)

--- src/endpoints/translation-endpoint.ts
+++ src/endpoints/translation-endpoint.ts
@@ -3,14 +3,17 @@ import { isArray } from 'util';
 import { EndpointModules } from './modules';
 import { EndpointTypes } from './types';
 import { InvalidParamError } from '../errors';
+import { TranslatorTypes, TranslatorModules } from '../translator';
 
 injectable(EndpointModules.Translation.Translate,
-  [ EndpointModules.Utils.WrapAync ],
-  async (wrapAsync: EndpointTypes.Utils.WrapAsync): Promise<EndpointTypes.Endpoint> => ({
+  [ EndpointModules.Utils.WrapAync,
+    TranslatorModules.Translate ],
+  async (wrapAsync: EndpointTypes.Utils.WrapAsync,
+    translate: TranslatorTypes.Translate): Promise<EndpointTypes.Endpoint> => ({
     uri: '/translate',
     method: EndpointTypes.EndpointMethod.GET,
     handler: [
-      wrapAsync((req, res, next) => {
+      wrapAsync(async (req, res, next) => {
         const queriesExpr = req.query.query;
         const from = req.query.from;
         const to = req.query.to;
@@ -23,11 +26,23 @@ injectable(EndpointModules.Translation.Translate,
         try {
           queries = JSON.parse(queriesExpr);
           if (isArray(queries) === false) throw new Error('');
+          queries.map((q: any) => {
+            if (!q.key || !q.message) throw new Error('');
+          });
         } catch (err) {
-          throw new InvalidParamError('query must be a stringified json-array');
+          throw new InvalidParamError('query must be a stringified json-array, (attr: key, message');
         }
 
-        res.status(200).json({});
+        const params: TranslatorTypes.TranslateParam[] =
+          queries.map((q: any) => ({
+            from: from,
+            to: to,
+            message: q.message,
+            key: q.key
+          }));
+        const translatedPromises = params.map((p) => translate(p));
+        const all = await Promise.all(translatedPromises);
+        res.status(200).json(all);
       })
     ]
   }));
